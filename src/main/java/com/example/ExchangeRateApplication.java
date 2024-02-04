6f5ad75142320a7670a6bc1cd3417a217af3a7d2
ManishMewara13 <manishmewara.sde@gmail.com>
2024-02-04 19:16:52 +0530

    added ExchangeRateApplication

---
+++ src/main/java/com/example/ExchangeRateApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.example")
+public class ExchangeRateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRateApplication.class, args);
+    }
+}
