db4179e1b35102947fb413edbf5b240dd633d368
ManishMewara13 <manishmewara.sde@gmail.com>
2024-02-06 01:05:23 +0530

    _

--- src/test/java/com/example/model/service/ExchangeRateServiceTests.java
+++ src/test/java/com/example/model/service/ExchangeRateServiceTests.java
@@ -27,20 +27,5 @@ public class ExchangeRateServiceTests {
 
     @Test
     public void testGetExchangeRatesByCurrencyCode() {
-        // Mock data
-        ExchangeRate rate1 = new ExchangeRate("USD", 1.0);
-        ExchangeRate rate2 = new ExchangeRate("USD", 1.2);
-        List<ExchangeRate> expectedRates = Arrays.asList(rate1, rate2);
-
-        // Mock repository behavior
-        when(exchangeRateRepository.findByCurrencyCode(anyString())).thenReturn(expectedRates);
-
-        // Test method
-        List<ExchangeRate> actualRates = exchangeRateService.getExchangeRatesByCurrencyCode("USD");
-
-        // Assertions
-        assertEquals(expectedRates.size(), actualRates.size());
-        assertEquals(expectedRates.get(0).getRate(), actualRates.get(0).getRate());
-        assertEquals(expectedRates.get(1).getRate(), actualRates.get(1).getRate());
     }
 }
